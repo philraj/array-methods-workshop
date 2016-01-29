7e8bfb5de72bc95af02a85de31b02cb4a823a3c4
Phil Rajchgot <tophil@outlook.com>
2016-01-29 18:31:06 +0000

    Added getPositives()

--- answers.js
+++ answers.js
@@ -7,4 +7,16 @@ function printPositives (array) {
     });
 }
 
+console.log("printPositives(): ");
 printPositives(nums);
+
+
+
+//Similar to the previous exercise, write a function called getPositives that takes an array and uses its filter method to return a new array with only the positive numbers
+function getPositives (array) {
+    return array.filter( function (value) {
+        if (value > 0) return true;
+    });
+}
+
+console.log("getPositives(): " + getPositives(nums));
