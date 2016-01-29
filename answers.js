30a7b96005490bd2b494492868c07951ee129f5b
Phil Rajchgot <tophil@outlook.com>
2016-01-29 18:45:34 +0000

    "Added" filterArray()... really just an alias to Array.prototype.filter

--- answers.js
+++ answers.js
@@ -20,3 +20,15 @@ function getPositives (array) {
 }
 
 console.log("getPositives(): " + getPositives(nums));
+
+
+
+/*Write a function called filterArray that takes an array AND a function as arguments. Your filter function should return a new array that contains only the elements where the passed function returns a truthy value.
+NOTE: You are allowed to use Array.prototype.filter to answer this question.
+NOTE 2: This is a bit of a trick question, the answer is a one-liner :)*/
+Array.prototype.filterArray = Array.prototype.filter;
+
+//output all numbers
+console.log("filterArray(): " + nums.filterArray( function(value) { 
+    return true;
+} ));
